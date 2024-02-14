91c18d9770211445a4627ae466013a795b21311e
kobayashian <systemdisgood@gmail.com>
2024-02-14 21:13:55 +0300

    Port changed to 8080

--- http_server.py
+++ http_server.py
@@ -8,7 +8,7 @@ def print_thread_func(argument):
 def socket_server_thread_func():
     # Define socket host and port
     server_host = '0.0.0.0'
-    server_port = 80
+    server_port = 8080
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind((server_host, server_port))
